50684a220bd7fa562be49a4fc86d58a31c043f31
Daler-web-dev <dsarifkulo021@gmail.com>
2022-06-09 20:47:58 +0500

    first comit

---
+++ index.js
@@ -0,0 +1,45 @@
+// let a = 22.49 // number
+// let b = 22.50 // number
+
+// int = parseInt(int)
+// int = parseFloat(int)
+// int = +int
+// Math.ceil(int) // up
+// Math.floor(int) // down
+// Math.round(int) // logical
+// int.toFixed(1)
+
+// a > b
+
+// Math.max(a, b)
+// Math.min(a,b)
+// Math.pow(7,2)
+// **
+
+// Math.random()
+// let rnd = Math.random()
+    
+
+
+
+// console.log(rnd);
+
+
+// increment
+// let int = 0
+
+// int = int + 1
+// int += 1
+// int++ // postFix
+// ++int // prefix
+
+// decrement
+// int = int - 1
+// int -= 1
+// int-- // postFix
+// --int // prefix
+
+// console.log(int);
+
+
+// github
